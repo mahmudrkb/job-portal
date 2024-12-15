d80612aeb7230e8d2f14343dabca068daf7ceb3f
mahmudrkb <almahmudrkb98@gmail.com>
2024-12-15 19:21:45 +0600

    project end

--- src/page/ViewApplications.jsx
+++ src/page/ViewApplications.jsx
@@ -1,14 +1,42 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const ViewApplications = () => {
   const applications = useLoaderData();
+  const handleStatusUpdate = (e, id) => {
+    console.log(e.target.value, id);
+
+    const data = {
+      status: e.target.value,
+    };
+
+    fetch(`http://localhost:5000/job-applications/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "Your work has been saved",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
 
   return (
     <div>
       <h1 className="text-3xl text-center">
         {" "}
-        this is view applications {applications.length}
+        This is view applications {applications.length}
       </h1>
 
       <div className="overflow-x-auto">
@@ -18,35 +46,40 @@ const ViewApplications = () => {
             <tr>
               <th>No</th>
               <th>Email</th>
-              <th>Favorite Color</th>
-            
+              <th>Select</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
-            {applications.map((job ,index) => (
-              <tr key={job._id}>
+            {applications.map((app, index) => (
+              <tr key={app._id}>
                 <td>
                   <div className="flex items-center gap-3">
                     <div className="avatar">
                       <div className="mask mask-squircle h-12 w-12">
-                       {index+1}
+                        {index + 1}
                       </div>
                     </div>
-                    <div>
-                      
-                    
-                    </div>
+                    <div></div>
                   </div>
                 </td>
                 <td>
-                 
-                  
-                  <div className="font-bold">{job.applicant_email}</div>
-           
+                  <div className="font-bold">{app.applicant_email}</div>
                 </td>
-                <td>Purple</td>
-               
+                <td>
+                  <select
+                    onChange={(e) => handleStatusUpdate(e, app._id)}
+                    defaultValue={app.status || "Change statues"}
+                    className="select select-bordered select-xs w-full max-w-xs"
+                  >
+                    <option disabled selected>
+                      Change statues
+                    </option>
+                    <option>Viva</option>
+                    <option>Hiring</option>
+                    <option>Rejected</option>
+                  </select>
+                </td>
               </tr>
             ))}
           </tbody>
